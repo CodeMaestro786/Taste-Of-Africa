68ffc292a81c841f6d2203bc87b0a9193065e6e3
CodeMaestro786 <153513766+CodeMaestro786@users.noreply.github.com>
2025-05-14 14:02:20 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,29 @@
 import React from 'react';
-function App() { return <div>TASTE OF AFRICA</div>; }
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main style={{ padding: '20px' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
 export default App;
