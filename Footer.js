b9092c5ec3d93359c8452ab46501c99900d89486
CodeMaestro786 <153513766+CodeMaestro786@users.noreply.github.com>
2025-05-14 14:05:23 +0200

    Update Footer.js

--- Footer.js
+++ Footer.js
@@ -1,1 +1,11 @@
-// Footer component placeholder
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} TASTE OF AFRICA. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
