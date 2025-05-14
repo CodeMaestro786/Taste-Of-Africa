9d752729f44da8d85a50c7d8afbe8ef34468849a
CodeMaestro786 <153513766+CodeMaestro786@users.noreply.github.com>
2025-05-14 14:04:51 +0200

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,1 +1,17 @@
-// Header component placeholder
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <h1>TASTE OF AFRICA</h1>
+      <nav>
+        <Link to="/">Home</Link> |{' '}
+        <Link to="/shop">Shop</Link> |{' '}
+        <Link to="/cart">Cart</Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
